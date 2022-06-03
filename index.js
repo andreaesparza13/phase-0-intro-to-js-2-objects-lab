005c135b858bb55a4c5f3245b5299d0b5ed57690
Andrea <acesparza05@gmail.com>
2022-06-02 22:36:27 -0500

    commit tree fb9962f3e9dcbca1083b737c8184330232661842

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, name) {
+    const minusKey = {...employee};
+    delete minusKey.name;
+    return minusKey;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+};
+
+const employee = {
+    name: "Andrea",
+    streetAddress: "855 Pride Ln",
+};
